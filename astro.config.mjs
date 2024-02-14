f14bf409cd0dd83642a7615a2f97c4ac00d367ce
Mati Tari <Matiastari@outlook.com.ar>
2024-02-14 17:53:10 -0300

    Add blog info, add @astrojs/mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
+import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), mdx()]
 });
